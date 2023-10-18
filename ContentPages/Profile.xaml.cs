e319a3725aba824dcf197f72b34d85efe41d9fa7
aldensprackling <alden.sprackling.dev@outlook.com>
2023-10-18 11:30:29 -0500

    Added some functionality and content to profile page. Also added an image.

--- ContentPages/Profile.xaml.cs
+++ ContentPages/Profile.xaml.cs
@@ -6,4 +6,17 @@ public partial class Profile : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void OnSaveClicked(object sender, EventArgs e)
+    {
+        // Implement your save logic here, e.g., save the settings to a data store.
+        string fullName = FullNameEntry.Text;
+        string emailAddress = EmailEntry.Text;
+        bool pushNotifications = PushNotificationSwitch.IsToggled;
+        bool emailNotifications = EmailNotificationSwitch.IsToggled;
+
+        ProfileNameLabel.Text = FullNameEntry.Text;
+
+        // You can save the settings to a database, preferences, or wherever is suitable for your app.
+    }
 }
